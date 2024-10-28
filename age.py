4305c8b878c12f70ad1b976a36fb1786a9cd42a8
Priyadarshini <145951339+D-Priyadarshini@users.noreply.github.com>
2024-10-28 20:05:11 +0530

    Create age.py

---
+++ age.py
@@ -0,0 +1,31 @@
+'''Ask a user for their birth year encoded as two digits (like "62") and for the current year, also encoded as two digits (like "99").
+Write a program to find the users current age in years.
+Input format:
+Input consists of 2 integers
+The first integer corresponds to the last 2 digits of the birth year
+The second integer corresponds to the last 2 digits of the current year
+Output format:
+Print the user's current age
+Refer below sample output for formatting.
+Sample Input:
+62
+00
+Sample Output:
+38
+'''
+birth = int(input())
+current = int(input())
+if current < birth:
+  birth = 100-birth
+  print(birth+current)
+else:
+  print(current - birth)
+
+
+
+
+
+
+
+
+
